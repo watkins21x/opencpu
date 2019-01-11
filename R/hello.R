793a88468f61120603fd9946862b3db2b40df23d
Isaac Medina Caballero <isaac.medina.caballero@gmail.com>
2019-01-11 11:30:34 -0600

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -4,77 +4,13 @@
 #' 
 #' @export
 #' @param myname your name. Required.
-#hello <- function(myname = ""){
-#  if(myname == ""){
-#    stop("Dime tu nombre!")
-#  }
-#  list(
-#    message = paste("hello", myname, "! This is", R.Version()$version.string)
-#  )
-#}
-
-hello<-function(myname = ""){
-  #library(RODBC)
-  library(RMySQL)
-  library(caret)
-  #channel <- odbcConnect("sumex")
-  #dataframe <- RODBC::sqlQuery(channel, "
-  #                      select fecha, hora, precio_zonal from
-  #                             sumex.pndmda
-  #                             where zona_carga = 'MEXICALI' order by
-  #fecha, hora")
-  #close(channel)
-  db_user <- 'root'
-  db_password <- 'sa12345678'
-  db_name <- 'sumex'
-  db_table <- 'pndmda'
-  db_host <- '127.0.0.1' # for local access
-  db_port <- 3306
-
-  # 3. Read data from db
-  mydb <-  dbConnect(MySQL(), user = db_user, password = db_password, dbname = db_name, host = db_host, port = db_port)
-  s <- paste0("select fecha, hora, precio_zonal from pndmda where zona_carga = 'MEXICALI' order by fecha, hora")
-  rs <- dbSendQuery(mydb, s)
-  dataframe <-  fetch(rs, n = -1)
-
-  on.exit(dbDisconnect(mydb))
-
-  x<-dataframe
-  k<-7
-  k<-k-1
-
-  FORE<-c()
-  for(j in 1:24){
-    PML <- x$precio_zonal[x$hora==j]
-    n<-length(PML)
-    DataPML <-data.frame()
-    for(i in 1:(n-k)){
-      A<-PML[i:(i+k)]
-      DataPML<-rbind.data.frame(DataPML,A)
-    }
-    Names<-c()
-    for(i in 0:k)
-      Names<-c(paste0('PML',i),Names)
-    colnames(DataPML)<-Names
-    DataPML$PML0[DataPML$PML0<=2000]<-0
-    DataPML$PML0[DataPML$PML0>2000]<-1
-    DataPML$PML0<-factor(DataPML$PML0)
-    training <- DataPML
-    trctrl <- trainControl(method = "repeatedcv", number = 15, repeats = 3)
-    svm_Linear <- train(PML0 ~., data = training, method = "svmLinear",
-                        trControl=trctrl,
-                        preProcess = c("center", "scale"),
-                        tuneLength = 10)
-    abc<-t(data.frame(tail(PML,k)))
-    colnames(abc)<-svm_Linear$coefnames
-    test_pred <- predict(svm_Linear, newdata = abc)
-    #print(test_pred)
-    FORE[j]<-levels(test_pred)[test_pred]
+hello <- function(myname = ""){
+  if(myname == ""){
+    stop("Dime tu nombre!")
   }
-  forecast<-data.frame(1:24,FORE)
-  colnames(forecast)<-c("Hora","Prediccion")
-  #return(forecast)
   list(
-    message = paste("hello, Si llego Aqui, ! This is", R.Version()$version.string)
-)
+    message = paste("hello", myname, "! This is", R.Version()$version.string)
+  )
 }
+
+
